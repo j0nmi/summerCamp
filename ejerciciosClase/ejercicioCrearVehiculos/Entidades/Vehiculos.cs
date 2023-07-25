5506e8007fb4d49c4ba664597b531d8c7c22783b
j0nmi <j0nmi@protonmail,com>
2023-07-25 18:25:22 +0200

    Mejorado codigo Vehiculos.cs

--- ejerciciosClase/ejercicioCrearVehiculos/Entidades/Vehiculos.cs
+++ ejerciciosClase/ejercicioCrearVehiculos/Entidades/Vehiculos.cs
@@ -1,28 +1,38 @@
 ﻿namespace Entidades
 {
-    public class Vehiculos
+    public class Vehiculo
     {
-        //  Crea un programa que permita crear diversos tipos de vehículos en función del constructor. 
-        //     -Permite crear vehículos especificando el año de compra y el color 
-        //     -Permite crear vehículos especificando marca y modelo 
-        //     -Permite crear vehículos especificando año de compra, marca, modelo y color.
+        // Al marcar una variable con ? -> Puede ser null
+        public int? AnoCompra { get; set; }
+        public string? Color { get; set; }
+        public string? Marca { get; set; }
+        public string? Modelo { get; set; }
 
+        // Creamos el ctor vacío
+        public Vehiculo() { }
 
-        public class Vehiculo
+        // Constructor con AnoCompra y Color
+        public Vehiculo(int anoCompra, string color)
         {
-            public int AnoCompra { get; set; }
-            public string Color { get; set; }
-            public string Marca { get; set; }
-            public string Modelo { get; set; }
-            public Vehiculo(int anocompra, string color, string marca, string modelo)
-            {
-                AnoCompra = anocompra;
-                Color = color;
-                Marca = marca;
-                Modelo = modelo;
-            }
+            AnoCompra = anoCompra;
+            Color = color;
 
         }
 
+        // Constructor para vehiculos con Marca y Modelo
+        public Vehiculo(string marca, string modelo)
+        {
+            Marca = marca;
+            Modelo = modelo;
+        }
+
+        // Ctor para vehiculos con AnoCompra , Color , Marca y Modelo
+        public Vehiculo(int anoCompra, string color, string marca, string modelo)
+        {
+            AnoCompra = anoCompra;
+            Color = color;
+            Marca = marca;
+            Modelo = modelo;
+        }
     }
 }

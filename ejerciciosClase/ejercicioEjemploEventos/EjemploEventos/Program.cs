aafd55631410d65455651f723480e63a7465673c
j0nmi <jmiguelmag@educacion.navarra.es>
2023-07-28 09:26:29 +0200

    Creado evento

--- ejerciciosClase/ejercicioEjemploEventos/EjemploEventos/Program.cs
+++ ejerciciosClase/ejercicioEjemploEventos/EjemploEventos/Program.cs
@@ -7,7 +7,9 @@
             Console.WriteLine("Hello, World!");
             // 1-. Crear instancia del reloj interno                     Publicador
             var reloj = new Reloj();
+
             // 2-. Crear reloj digital y nos suscribimos                 Suscriptor
+
             // 3-. Poner en marcha el reloj
         }
     }

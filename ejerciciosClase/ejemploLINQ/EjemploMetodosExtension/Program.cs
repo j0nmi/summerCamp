97a2c6424c13ec09f40948ad56d98c3c0eb0307d
j0nmi <jmiguelmag@educacion.navarra.es>
2023-07-31 09:45:34 +0200

    Update metodos de extension

---
+++ ejerciciosClase/ejemploLINQ/EjemploMetodosExtension/Program.cs
@@ -0,0 +1,48 @@
+﻿using Extensiones;
+namespace EjemploMetodosExtension
+{
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string juan = "JuAN lOpEz".ConvertirCadena();
+            string luis = "lUiS MaRTinEz".ConvertirCadena();
+            var saludo = "Hola Mundo !!!!!";
+
+            var convertir = saludo.ConvertirCadena();
+
+
+            // Mostrar año de la fecha
+            var fecha = DateTime.Now;   
+            var año = fecha.ObtenerAño();
+
+
+            // Agregar metodo de extension para comparar dos enteros
+            int a = 1;
+            int b = 2;
+
+            if (a.EsMayorQue(b))
+            {
+                Console.WriteLine($"{a} es mayor que {b}");
+            }
+            else
+            {
+                Console.WriteLine($"{a} es menor que {b}");
+            }
+
+            // Obtener el primer elemento de la lista
+            var lista = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            var primero = lista.Primero();
+            Console.WriteLine($"{primero}");
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ejerciciosClase/ejemploLINQ/Extensiones/Utilidades.cs
@@ -0,0 +1,35 @@
+﻿namespace Extensiones
+{
+    public static class Utilidades
+    {
+
+        public static string ConvertirCadena(this string cadena)
+        {
+            return cadena.Trim().ToUpper() + ".";
+        }
+
+
+        public static int ObtenerAño(this DateTime dateTime)
+        {
+            return dateTime.Year;
+        }
+
+
+        public static bool EsMayorQue(this int numero1, int numero2)
+        {
+            return numero1 > numero2;
+        }
+
+        public static int Primero(this List<int> lista)
+        {
+            //foreach (int i in lista)
+            //{
+            //    return lista.IndexOf(i);
+            //}
+
+            return lista.FirstOrDefault();
+
+            // return lista[0];
+        }
+    }
+}

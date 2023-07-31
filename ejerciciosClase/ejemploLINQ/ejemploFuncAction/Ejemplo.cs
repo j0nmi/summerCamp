3394ceb7e95b513df466141c9845a6a420846e8f
j0nmi <jmiguelmag@educacion.navarra.es>
2023-07-31 10:14:29 +0200

    Suma 2 numeros Lambda

---
+++ ejerciciosClase/ejemploLINQ/ejemploFuncAction/Ejemplo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemploFuncAction
+{
+    internal class Ejemplo
+    {
+        internal void EjemploDelegados1()
+        {
+            // Expresión Lambda mediante Func<>
+            Func<string> holaMundoExpresion =
+
+                // 1-. Parametros
+                ()
+                // 2-. Operador Lambda
+                =>
+                // 3-. Delegado / Método anónimo / Expresión
+                "Hola Mundo !!";
+
+            Console.WriteLine(holaMundoExpresion);
+            Console.WriteLine(holaMundoExpresion());
+
+            // Expresion Lambda mediante Bloque Func<>
+            Func<string> holaMundoBloque =
+
+                // Parametros
+                ()
+                // Operador Lambda
+                =>
+                // Delegado / Método anónimo 
+                {
+                    var cadena = "Hola Mundo !!!";
+                    return cadena;
+                };
+
+            Console.WriteLine(holaMundoBloque());
+
+            // Crear una expresión lambda que sume 2 numeros
+            // Expresión Lambda mediante Func<>
+            Func<int, int, int> sumar = (a, b) => { var suma = a + b; return suma; };
+            // 1 int: Primer param
+            // 2 int: Segundo param
+            // 3 int: return de la expresión lambda
+            // Recibe 2 parametros de tipo entero (a,b)
+            // Devuelve la suma de los dos parámetros
+            Console.WriteLine(sumar(10,20));
+        }
+
+
+
+    }
+}
---
+++ ejerciciosClase/ejemploLINQ/ejemploFuncAction/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ejemploFuncAction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ejemplo = new Ejemplo();
+            ejemplo.EjemploDelegados1();
+            
+        }
+    }
+}

d9b2168192fd2e802ab503cc1fbb84b85a43ead1
j0nmi <jmiguelmag@educacion.navarra.es>
2023-08-01 09:53:31 +0200

    Ejemplo LINQ Básico

---
+++ ejerciciosClase/ejemploLINQ/EjLinq/EjemploBasico.cs
@@ -0,0 +1,42 @@
+﻿namespace EjLinq
+{
+    internal class EjemploBasico
+    {
+        public EjemploBasico()
+        {
+        }
+
+        public void Ejecutar()
+        {
+            string[] niveles = { "Básico", "Intermedio", "Avanzado" };
+            int nc = niveles.Count();
+
+            // Seleciona los niveles cuya longitud en caracteres es > 6
+            var listaConsultaForeach = new List<string>();
+            foreach (string nivel in niveles)
+            {
+                if (nivel.Length > 6)
+                {
+                    listaConsultaForeach.Add(nivel);
+                }
+            }
+
+            // Sintaxis con SQL
+            // Ordenado por nombre de nivel
+            var consultaLinq = from nivel in niveles
+                               where nivel.Length > 6
+                               orderby nivel
+                               select nivel;
+
+            // Obtener resultados
+            List<string> listaResultado = consultaLinq.ToList();
+
+            // Sintaxis de Metodos
+            // 1-. Preparación
+            var consultaLinqMetodos = niveles.Where(nivel => nivel.Length > 6).ToList();
+
+            // 2-. Resultados
+
+        }
+    }
+}
---
+++ ejerciciosClase/ejemploLINQ/EjLinq/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EjLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ejemploBasico = new EjemploBasico();
+            ejemploBasico.Ejecutar();
+        }
+    }
+}

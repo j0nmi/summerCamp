129e4a11bc9ef8fe9cecfa469de3ae479613752b
j0nmi <jmiguelmag@educacion.navarra.es>
2023-08-03 10:06:55 +0200

    Creado proyecto vacío

--- BLOQUE2/ejerciciosClase/ProyectoWebSolucion/ProyectoWebVacio/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProyectoWebVacio.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}

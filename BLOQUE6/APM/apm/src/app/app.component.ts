bac49951fdefe16ef66656b8c2debe5e381b33c9
j0nmi <jmiguelmag@educacion.navarra.es>
2023-08-29 13:03:26 +0200

    Update

--- BLOQUE6/APM/apm/src/app/app.component.ts
+++ BLOQUE6/APM/apm/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pm-root',
   template: `
-  <div>
+  <div class="container mt-4">
   <pm-navbar></pm-navbar>
     <h1>{{pageTitle}}</h1>
     <h3>Moneda base del conversor: {{codigoMonedaBase}}</h3>
@@ -16,6 +16,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   pageTitle = 'Conversor de monedas ACME v.2023';
-  codigoMonedaBase = 'EUR';
+  codigoMonedaBase = 'USD';
   title = 'apm';
 }

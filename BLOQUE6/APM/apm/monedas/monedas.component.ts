c38715962e72073aca319b84fd24c903d7d1d501
j0nmi <jmiguelmag@educacion.navarra.es>
2023-08-29 10:49:32 +0200

    templateUrl, Bootstrap

--- BLOQUE6/APM/apm/monedas/monedas.component.ts
+++ BLOQUE6/APM/apm/monedas/monedas.component.ts
@@ -2,18 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'pm-monedas',
-  template: `
-    <div>
-      <h3>Lista de Monedas</h3>
-      <ul>
-        <li>Dólar estadounidense (USD)</li>
-        <li>Euro (EUR)</li>
-        <li>Libra esterlina (GBP)</li>
-        <li>Yen japonés (JPY)</li>
-        <li>Dólar australiano (AUD)</li>
-      </ul>
-    </div>
-  `
+  templateUrl: 'monedas.component.html'
 })
 
 export class MonedasComponent {
